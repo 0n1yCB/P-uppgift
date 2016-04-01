2062ef1fa7219045a634a54d212c1659d3b04a96
Anton Lu <anton96lu@gmail.com>
2016-04-01 11:31:42 +0200

    Final commit
    Revised, working methods.

--- P-uppgift.py
+++ P-uppgift.py
@@ -1,17 +1,8 @@
-from queueADT import *
+#from queueADT import *
+from functions import *
 import classes
 import time
 import random
-
-def noErrands():
-	errands = 1
-	while random.randrange(0,2) == 1:
-		errands += 1
-	return errands
-
-def newCustomer():
-	if random.randrange(0,6) == 5:
-		return True
 
 def enterCustomer():
 	global beingServed
@@ -20,61 +11,63 @@ def enterCustomer():
 	global storeOpen
 	global queue
 
+	def newCustomer():
+		if random.randrange(0,5) == 0:
+			return True
+
 	if newCustomer() and storeOpen:														# kollar varje minut om en nu kund kommer in
 		if queue.isEmpty() and not beingServed:
-			customer = classes.person(kundnr)
-			beingServed = True
-			print(time , " kommer kund " , customer.kundnr , " in och blir genast betjänad.")
+			customer = classes.person(kundnr, -1)
+			print(time , "kommer kund" , customer.kundnr , "in och blir genast betjänad.")
 			kundnr += 1
 			beingServed = True
-#			return queue, beingServed, customer, kundnr
+
 		else:												# annars ställs personen i kön
 			queue.put(classes.person(kundnr))
 			kundnr += 1
-			print(time , " kommer kund " , kundnr-1 , " in och ställer sig i kön som nr." , queue.length)
-#			return queue, beingServed, None, kundnr
-#			funktionsargument: storeOpen, queue, beingServed, kundnr, time
+			print(time , "kommer kund" , kundnr-1 , "in och ställer sig i kön som nr." , queue.length)
+
 
 def updateCheck():
 	global beingServed
-	global consumedTime
 	global time
 	global customer
 	global queue
+	global queueTime
 
 	if beingServed == True:									# om en kund betjänas så ökas tiden det har tagit för ärendena
-		if consumedTime >= 2*customer.errands:				# om den konsumerade tiden nått 2 * ärenden minuter lämnar kunden disken och en kund hämtas från kön.
+		customer.consumedTime += 1
+		if customer.canLeave():				# om den konsumerade tiden nått 2 * ärenden minuter lämnar kunden disken och en kund hämtas från kön.
 			if not queue.isEmpty():
-				print(time , " går kund " , customer.kundnr , " och kund " , customer.kundnr+1 , "blir betjänad.")
+				print(time , "går kund" , customer.kundnr , "och kund" , customer.kundnr+1 , "blir betjänad.")
 				customer = None
 				customer = queue.get()
-				consumedTime = 0
 			else:
-				print(time , " går kund ", customer.kundnr)
+				print(time , "går kund", customer.kundnr)
 				beingServed = False
-		else:
-			consumedTime += 1
-	#return queue, beingServed, customer, consumedTime
-	#funktionsargument: queue, beingServed, customer, time, consumedTime
 
 def timeCheck():
 	global time
 	global storeOpen
+	global queue
 
 	if time.current == 1080:
-		print("Dörren stängdes 18:00")
+		print("Dörren stängdes 18:00.")
 		storeOpen = False
+
+	if not queue.isEmpty():
+		queue.queueTime += 1
+
+
 
 time = classes.time() # kl 9:00
 kundnr = 1
-consumedTime = 0
 storeOpen = True
 beingServed = False
-
-queue = queue()
-
+queue = classes.queue()
 
 while True:
+
 	enterCustomer()
 
 	updateCheck()
@@ -86,6 +79,6 @@ while True:
 
 	time.current += 1
 
-#	print(time.current, storeOpen, queue.length, consumedTime, beingServed)
+	#	print(time.current, storeOpen, queue.length, consumedTime, beingServed)
 
-print("STATISTIK: " , kundnr , "kunder betjänades.")
+print("STATISTIK:" , kundnr , "kunder betjänades. Kundväntetid", queue.queueTime, "minuter =>", int(60*queue.queueTime/kundnr), "s per kund.")
--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-from functions import noErrands
+import random
 
 class time():
 	def __init__(self):
@@ -23,6 +23,57 @@ class time():
 		return str("Kl. " + str(hour) + ":" + minute)
 
 class person():
-	def __init__(self, custNo = 0):
+	def __init__(self, custNo = 0, consumedTime = 0):
+		def noErrands():
+			errands = 1
+			while random.randrange(0,2) == 1:
+				errands += 1
+			return errands
+
 		self.errands = noErrands()
 		self.kundnr = custNo
+		self.consumedTime = consumedTime
+
+	def canLeave(self):
+		return self.consumedTime >= 2*self.errands
+
+class Node():
+	def __init__(self, item, nextitem = None):
+		self.item = item
+		self.next = nextitem
+
+class queue():
+	def __init__(self):
+		self.top = None
+		self.last = None
+		self.length = 0
+		self.queueTime = 0
+
+	def put(self, inputdata):
+		self.length += 1
+		if self.isEmpty():
+			self.top = Node(inputdata)
+			self.last = self.top
+			self.top.next = self.last
+		else:
+			self.last.next = Node(inputdata)
+			self.last = self.last.next
+
+	def get(self):
+		if self.isEmpty():
+			return None
+		else:
+			out = self.top.item
+			self.length = self.length -1
+			if self.top == self.last:
+				self.last = None
+				return self.top.item
+
+			self.top = self.top.next
+			return out
+
+	def isEmpty(self):
+		if self.last == None:
+			return True
+		else:
+			return False
